b8538022582d3167ebe35c3ca1729ed173aac722
henry viirmäe <heviir@ttu.ee>
2021-06-10 21:14:33 +0300

    plugin metadata + child logger

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,50 @@
-import { FastifyInstance, FastifyPluginCallback } from 'fastify';
+import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
-const fastifyExitHandler: FastifyPluginCallback = (app, _, done) => (
-  (
-    [
-      'SIGHUP',
-      'SIGINT',
-      'SIGQUIT',
-      'SIGKILL',
-      'SIGTRAP',
-      'SIGABRT',
-      'SIGBUS',
-      'SIGFPE',
-      'SIGUSR1',
-      'SIGSEGV',
-      'SIGUSR2',
-      'SIGTERM',
-    ] as const
-  ).forEach(signal => process.once(signal, signal => handleExit(signal, undefined, 0, app))),
-  process.once('uncaughtException', error => handleExit(undefined, error, 0, app)),
-  process.once('unhandledRejection', (error, promise) =>
-    handleExit(undefined, new UnhandledRejectionError(error, promise), 0, app),
-  ),
-  app.decorate('exit', (code: number, error?: Error) => handleExit(undefined, error, code, app)),
-  done()
+export default fp(
+  async app => {
+    const log = app.log.child({ plugin: 'fastify-exit-handler' });
+    (
+      [
+        'SIGHUP',
+        'SIGINT',
+        'SIGQUIT',
+        'SIGKILL',
+        'SIGTRAP',
+        'SIGABRT',
+        'SIGBUS',
+        'SIGFPE',
+        'SIGUSR1',
+        'SIGSEGV',
+        'SIGUSR2',
+        'SIGTERM',
+      ] as const
+    ).forEach(signal => process.once(signal, signal => handleExit(signal, undefined, 0, app, log)));
+    process.once('uncaughtException', error => handleExit(undefined, error, 0, app, log));
+    process.once('unhandledRejection', (error, promise) =>
+      handleExit(undefined, new UnhandledRejectionError(error, promise), 0, app, log),
+    );
+    app.decorate('exit', (code: number, error?: Error) => handleExit(undefined, error, code, app, log));
+  },
+  { name: 'fastify-exit-handler' },
 );
-
-export default fp(fastifyExitHandler);
 
 async function handleExit(
   signal: NodeJS.Signals | undefined,
   err: Error | undefined,
   code: number,
   app: FastifyInstance,
+  log: FastifyInstance['log'],
 ) {
-  if (signal) app.log.info({ signal }, 'caught NodeJS exit signal');
-  if (err) app.log.info({ err }, 'error caused process to exit');
-  app.log.info('shutting down app...');
+  if (signal) log.info({ signal }, 'caught NodeJS exit signal');
+  if (err) log.info({ err }, 'error caused process to exit');
+  log.info('shutting down app...');
   try {
     await app.close();
   } catch (error) {
-    app.log.error(error, 'failed to shut down app gracefully');
+    log.error(error, 'failed to shut down app gracefully');
   }
-  app.log.info('exiting process. Goodbye!');
+  log.info('exiting process. Goodbye!');
   process.exit(code);
 }
 
